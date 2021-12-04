1c2d540a452934b1e474cd4ff89f47e8e86a85ec
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-12-04 18:22:49 +0100

    Spent 3 hours for complete all ex02! I'm Happy ;)

--- src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
+++ src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -30,43 +31,95 @@ public final class MusicGroupImpl implements MusicGroup {
     }
 
     @Override
-    public Stream<String> orderedSongNames() {
-        return null;
+    public Stream<String> orderedSongNames() {	
+    	final Set<String> mySong = new HashSet<>();
+    	this.songs.forEach(t -> {
+    		mySong.add(t.getSongName());
+    	});
+    	//System.out.println(mySong);
+        return mySong.stream().sorted();
     }
 
     @Override
     public Stream<String> albumNames() {
-        return null;
+    	final Set<String> myAlbum = new HashSet<>();
+    	this.albums.forEach((k, v) -> {
+    		myAlbum.add(k);
+    	});
+        return myAlbum.stream();
     }
 
     @Override
     public Stream<String> albumInYear(final int year) {
-        return null;
+    	final Set<String> albumYear = new HashSet<>();
+    	this.albums.forEach((k, v) -> {
+    		if (v.equals(year)) {
+    			albumYear.add(k);
+    		}
+    	});
+        return albumYear.stream();
     }
 
     @Override
     public int countSongs(final String albumName) {
-        return -1;
+    	final Set<String> counterSong = new HashSet<>();
+    	this.songs.forEach(t -> {
+    		if (t.albumName.filter(name -> name.equals(albumName)).isPresent()) {
+    			counterSong.add(t.getSongName());
+    		}
+    	});
+    	//System.out.println(counterSong);
+        return (int) counterSong.stream().count();
     }
 
     @Override
     public int countSongsInNoAlbum() {
-        return -1;
+    	final Set<String> countSongNoAlbum = new HashSet<>();
+    	this.songs.forEach(t -> {
+    		if (t.albumName.isEmpty()) {
+    			countSongNoAlbum.add(t.getSongName());
+    		}
+    	});
+        return (int) countSongNoAlbum.stream().count();
     }
 
     @Override
     public OptionalDouble averageDurationOfSongs(final String albumName) {
-        return null;
+    	final Set<Double> averageDurSong = new HashSet<>();
+    	this.songs.forEach(t -> {
+    		if (t.albumName.filter(name -> name.equals(albumName)).isPresent()) {
+    			averageDurSong.add(t.duration);
+    		}
+    	});
+        return OptionalDouble.of(averageDurSong.stream().collect(Collectors.averagingDouble(x -> x)));
     }
 
     @Override
     public Optional<String> longestSong() {
-        return null;
+    	final Map<Optional<Double>, String> longSong = new HashMap<>();
+    	final Set<Double> durSong = new HashSet<>();
+    	this.songs.forEach(t -> {
+    		longSong.put(Optional.of(t.getDuration()), t.getSongName());
+    		durSong.add(t.getDuration());
+    	});
+    	Optional<Double> max = durSong.stream().max((Double::compare));
+    	Optional<String> name = Optional.ofNullable(longSong.get(max));
+    	//System.out.println(name);
+        return name;
     }
 
     @Override
     public Optional<String> longestAlbum() {
-        return null;
+    	final Map<Optional<Double>, String> longAlbum = new HashMap<>();
+    	final Set<Double> durAlbum = new HashSet<>();
+    	this.albums.forEach((k, v) -> {
+    		longAlbum.put(Optional.of(averageDurationOfSongs(k).getAsDouble()), k);
+    		durAlbum.add(averageDurationOfSongs(k).getAsDouble());
+    	});
+    	Optional<Double> max = durAlbum.stream().max((Double::compare));
+    	Optional<String> name = Optional.ofNullable(longAlbum.get(max));
+    	//System.out.println(name);
+        return name;
     }
 
     private static final class Song {
